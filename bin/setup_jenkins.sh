52835bcf8e8263807a12cf6f53cb124b9a84f2c1
mikelo1 <mosinalde@atsistemas.com>
2019-06-13 14:00:51 +0200

    Add GUID #2

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -19,7 +19,7 @@ oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi
 # Create custom agent container image with skopeo
 oc new-build  -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n
       USER root\nRUN yum -y install skopeo && yum clean all\n
-      USER 1001' --name=jenkins-agent-appdev
+      USER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
 echo "apiVersion: v1
